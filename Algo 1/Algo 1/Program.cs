2b1a5eb7da75ef51e721eb7a4db2859cf8ee819c
Tchandra-17 <tchandra.camacho193@tectijuana.edu.mx>
2019-10-10 09:08:01 -0700

    Este es mi primer push desde mi repo local hacia mi repo de la nube

---
+++ Algo 1/Algo 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola obtuso, adivina. Este es mi primer repositorio. Soy un hackerman");
+        }
+    }
+}
