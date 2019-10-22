907b63168a9352a3b9a23fa2f214d83a9d977683
Tchandra-17 <tchandra.camacho193@tectijuana.edu.mx>
2019-10-22 09:28:21 -0700

    Este es mi primer commit con el profe :)

---
+++ Practica_Arreglos/Practica_Arreglos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola slñdkgjñasdjfasldkjfalkdsjfañsidfjñalskdjfñalsjk");
+
+            Console.ReadKey();
+        }
+    }
+}
